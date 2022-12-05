54fd097ebacf9385a2e8684fb7df146d86e73b93
Pavlo Nakonechny <117309523+Pavlo2002@users.noreply.github.com>
2022-12-05 23:06:43 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+// // Task 1
+
+let city = "Київ";
+city = "Львів";
+let address = city;
+alert(address)
+
+// Task 2
+
+let n = 20;
+let result = Math.pow(n,3);
+console.log(result);
+
+// Task 3
+
+let a = 4;
+let b = 6;
+let c = 5.25;
+let d = 3.5;
+let e = c * d;
+let g = a * b;
+document.write(e + g);
